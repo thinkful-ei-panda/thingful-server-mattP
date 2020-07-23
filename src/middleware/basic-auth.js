5af4ce90cad77e0b5b701928c210f57ef009550d
Matt <63121708+themlp101@users.noreply.github.com>
2020-07-23 14:31:10 -0400

    basic auth async await refactor

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,49 +1,59 @@
 const bcrypt = require('bcryptjs')
 const AuthService = require('../auth/auth-service')
 
-const requireAuth = (req, res, next) => {
-	const authToken = req.get('Authorization') || ''
+const requireAuth = async (req, res, next) => {
+	try {
+		const authToken = req.get('Authorization') || ''
 
-	let basicToken
+		let basicToken
 
-	if (!authToken.toLowerCase().startsWith('basic')) {
-		return res.status(401).json({ error: `Missing basic token` })
-	} else {
-		basicToken = authToken.slice(
-			'basic '.length,
-			authToken.length
+		if (!authToken.toLowerCase().startsWith('basic')) {
+			return res
+				.status(401)
+				.json({ error: `Missing basic token` })
+		} else {
+			basicToken = authToken.slice(
+				'basic '.length,
+				authToken.length
+			)
+		}
+
+		const [
+			tokenUsername,
+			tokenPassword,
+		] = AuthService.parseBasicToken(basicToken)
+
+		if (!tokenUsername || !tokenPassword) {
+			return res
+				.status(401)
+				.json({ error: `Unauthorized request` })
+		}
+
+		const user = await AuthService.getUserWithUserName(
+			req.app.get('db'),
+			tokenUsername
 		)
+
+		if (!user) {
+			return res
+				.status(401)
+				.json({ error: `Unauthorized request` })
+		}
+		const passwordMatch = await bcrypt.compare(
+			tokenPassword,
+			user.password
+		)
+
+		if (!passwordMatch) {
+			return res
+				.status(401)
+				.json({ error: `Unauthorized request` })
+		}
+		req.user = user
+		return next()
+	} catch (error) {
+		next(error)
 	}
-
-	const [
-		tokenUsername,
-		tokenPassword,
-	] = AuthService.parseBasicToken(basicToken)
-
-	if (!tokenUsername || !tokenPassword) {
-		return res.status(401).json({ error: `Unauthorized request` })
-	}
-
-	AuthService.getUserWithUserName(req.app.get('db'), tokenUsername)
-		.then((user) => {
-			if (!user) {
-				return res
-					.status(401)
-					.json({ error: `Unauthorized request` })
-			}
-			return bcrypt
-				.compare(tokenPassword, user.password)
-				.then((passwordMatch) => {
-					if (!passwordMatch) {
-						return res
-							.status(401)
-							.json({ error: `Unauthorized request` })
-					}
-					req.user = user
-					next()
-				})
-		})
-		.catch(next)
 }
 
 module.exports = { requireAuth }
